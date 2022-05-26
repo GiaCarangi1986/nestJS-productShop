426edde23b7548343cf15af740fb024266a730db
LizaKurochkina <dasha.71.99@mail.ru>
2022-05-26 12:05:56 +0300

    fix

--- src/checks/check.service.ts
+++ src/checks/check.service.ts
@@ -304,6 +304,7 @@ export class CheckService {
   ): Promise<CheckLine[]> {
     const checks = await this.checkRepository.find({
       where: {
+        isCancelled: false,
         parentCheckId: null,
         paid: true,
         dateTime: Raw(
